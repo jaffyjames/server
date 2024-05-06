da1c7cd573da6e0cdd42e685a5314f637bf21653
jaffyjames <165079752+jaffyjames@users.noreply.github.com>
2024-05-06 17:08:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const express = require("express");
+const http = require("http"); // Import the http module
+const app = express();
+const PORT = process.env.PORT || 3008;
+
+// paste your IP address of the local network
+//const HOST = "192.168.4.22";
 //const HOST = "192.168.1.10";
 
 // paste your IP address of the local network
@@ -5,7 +12,7 @@
 
 const HOST = process.env.HOST || '0.0.0.0'; // Default to listening on all interfaces
 
-const server = require("http").createServer(app); // Use http module instead of https
+const server = http.createServer(app); // Use http module instead of https
 const io = require("socket.io")(server);
 
 app.use(express.static("public"));
